d825342e0b26fd96e5b366e0071dd960da58d60b
Natalie Eidt <neidt@yahoo.com>
2019-02-05 11:05:48 -0600

    done for wednesday

---
+++ Assets/RunGameButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RunGameButtons : MonoBehaviour
+{
+    public Canvas pauseMenu;
+
+    public void Run()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
